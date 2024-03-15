bdba9bdf323d045accaaeff5384c84bba1b844b3
Andrew Zamler-Carhart <andrewzc@synamedia.com>
2024-03-15 16:56:44 -0400

    Formatting changes.

--- videoManager.js
+++ videoManager.js
@@ -2,14 +2,9 @@ import { remotePlayer, lifecycle } from "@Synamedia/hs-sdk";
 
 class VideoManager {
 
-  // localPlayer;
-  
-  media() {
-    return this.localPlayer.getMediaElement();
-  }
-  
   init(player) {
     this.localPlayer = player;
+    this.remotePlayer = remotePlayer;
  
     remotePlayer.addEventListener("timeupdate", () => {
       this.media().currentTime = remotePlayer.currentTime || 0;
@@ -37,16 +32,20 @@ class VideoManager {
     }
   }
   
+  media() {
+    return this.localPlayer.getMediaElement();
+  }
+
   play() {
     this.media().play().catch(error => {
       console.log("Unable to play video. Possibly the browser will not autoplay video with sound.");
     });
   }
-  
+
   pause() {
     this.media().pause();
   }
-  
+
   playPause() {
     if (this.media().paused) {
       this.play();
@@ -54,7 +53,7 @@ class VideoManager {
       this.pause();
     }
   }
-  
+
   skip(seconds) {
     this.media().currentTime = this.media().currentTime + seconds;
   }
@@ -68,7 +67,7 @@ class VideoManager {
     remotePlayer.currentTime = currentTime;
     remotePlayer.play();
   }
-  
+
   async toggleBackground() {
     const currentState = await lifecycle.getState();
     if (currentState == "background" || currentState == "inTransitionToBackground") {
